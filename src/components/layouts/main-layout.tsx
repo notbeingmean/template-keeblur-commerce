66cc3f4f12d969225eb2c1005360917700b576d3
notbeingmean <jirateep.loyma02@gmail.com>
2024-11-02 09:24:04 +0700

    formating

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import ContainerWrapper from "../wrappers/container";
 import Navbar from "./navbar/navbar";
-import { ChevronDown, Mail, PhoneCall } from "lucide-react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import Image from "next/image";
 import TopNavigation from "./navbar/top-nav";
 
 type MainLayoutProps = {
