b94e9369b7172d902773058eed9e1b27088488ee
Jirateep Loyma <jirateep.loyma02@gmail.com>
2024-11-03 19:46:39 +0700

    add - utils lib

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,56 @@
+import { client } from "@/server/client";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export async function fetchCategories(
+  opts: {
+    skip?: number;
+    take?: number;
+    productSkip?: number;
+    productTake?: number;
+  } = {}
+) {
+  const { data, error } = await client.api.categories.get({
+    query: opts,
+  });
+
+  if (error) {
+    return [];
+  }
+
+  return data;
+}
+
+export async function fetchProducts(opts: { skip?: number; take?: number }) {
+  const { data, error } = await client.api.products.get({
+    query: opts,
+  });
+
+  if (error) {
+    return [];
+  }
+
+  return data;
+}
+
+export async function fetchSearchResults(searchValue: string) {
+  const { data, error } = await client.api.autocomplete.get({
+    query: {
+      keyword: searchValue,
+    },
+  });
+
+  if (error) {
+    return [];
+  }
+
+  return data;
+}
+
+export type CategoryData = Awaited<ReturnType<typeof fetchCategories>>;
+export type ProductType = Awaited<ReturnType<typeof fetchProducts>>;
+export type SearchResults = Awaited<ReturnType<typeof fetchSearchResults>>;
