7d87a5ca6c43124d096df86fc7be047cf4fcaf30
Jirateep Loyma <jirateep.loyma02@gmail.com>
2024-11-21 18:44:38 +0700

    update - database schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -62,6 +62,23 @@ async function main() {
     ],
   });
 
+  await prisma.product.create({
+    data: {
+      name: "Keychron K3",
+      price: 3000,
+      description: "",
+      stock: 0,
+      slug: "keychron-k3",
+      productDetail: {
+        create: {
+          name: "Keychron K3",
+          value:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta, odio at consectetur rutrum, magna orci commodo ante, ac viverra sapien mi sed felis. Sed tempor ultricies leo, id varius erat bibendum eget. Donec at nisl id justo iaculis euismod ut at elit. Suspendisse vehicula, mauris eget auctor egestas, enim risus accumsan dui, ut elementum neque mi vel erat. Fusce pharetra erat est, sit amet sollicitudin massa finibus eu. Maecenas id dolor congue, tincidunt libero ac, vestibulum mi. Sed nunc magna, mollis ac est bibendum, dictum tempor mi. Donec sodales egestas justo, nec accumsan elit malesuada id. Donec lobortis, felis nec sollicitudin ultricies, tortor elit pulvinar erat, ut tincidunt turpis erat at lorem. Suspendisse velit neque, placerat condimentum elit vitae, rutrum maximus diam. Nulla rhoncus rhoncus justo, ut hendrerit ex mollis nec. Ut urna diam, vehicula nec posuere eget, vestibulum vitae sem. Donec sollicitudin orci non sem viverra, quis laoreet odio consectetur. Integer auctor dolor ut ornare pulvinar. Donec ac vehicula quam.",
+        },
+      },
+    },
+  });
+
   console.log("Data seeded successfully");
 }
 
